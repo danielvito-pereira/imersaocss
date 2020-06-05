edd91e3a3615096ee9e2740e676ced61a4da3a81
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-06-05 12:25:55 -0300

    script do otão
    nesse comit foi adicionado o botão

---
+++ script/js.js
@@ -0,0 +1,15 @@
+
+window.onscroll = function() {/*é chamada assim que a pagina for rolada */
+    scroll();
+}
+function scroll() {/* mostra o botão assim que decer a pagina */
+    let btn = document.getElementById("topo");/*pega o botão*/
+    if (document.documentElement.scrollTop > 50) {/* unidade de medida, que mede o quanto de scroll foi dado a partir do topo */
+        btn.style.display = "block";/*passa o botão de none para block */
+    } else {
+        btn.style.display = "none";
+    }
+}
+function backToTop() {/*faz o otão voltar ao topo que vai ser chamada dentro da função onclick do botão  */
+    document.documentElement.scrollTop = 0;
+}
